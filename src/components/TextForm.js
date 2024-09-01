b76f47087f51d15423d9184158fdd3e0ad5e2071
Abhang <abhangmehendale@gmail.com>
2024-09-02 00:52:38 +0530

    video 8 done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,13 +6,20 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   }
+  const handleLoClick = () => {
+    /* console.log("Hello"+ text); */
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange = (event) => {
     /* console.log("On Change"); */
     setText(event.target.value)
   }
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   return (
-    <div className="mb-3">
+    <>
+   <div className="container">
+   <div className="mb-3">
       <h3>{props.heading}</h3>
       <div className="mb-3">
         <textarea
@@ -23,9 +30,24 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase{" "}
+      <button className="btn btn-primary mx-3" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+      <button className="btn btn-primary" onClick={handleLoClick}>
+        Convert to Lowercase
       </button>
     </div>
+    <div className="container">
+        <h1>Your Text Summary</h1>
+        <p> {text.split(" ").length} Words and {text.length} characters</p>
+        <p> {0.008*text.split(" ").length} Minutes to read on an average</p>
+        
+    </div>
+    <div className="container">
+        <p> Preview </p>
+        <p>{text}</p>
+        </div>
+    </div>
+    </>
   );
 }
